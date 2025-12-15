bdec9845adb09f838e375a150c98c73b9d0c2775
Fatih Bıyıklı <fatihbiyiklio@hotmail.com>
2025-12-16 01:40:59 +0300

    refactor: Remove manual PWA service worker registration and update build asset hashes.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppProvider } from './context/AppContext'
 import './index.css'
-// @ts-ignore
-import { registerSW } from 'virtual:pwa-register'
+import './index.css'
 
-registerSW({ immediate: true })
+// VitePWA plugin with 'autoUpdate' injects the registration script automatically.
+// We don't need to manually import and call registerSW unless we want custom behavior.
+// Let's rely on the plugin's injection to be safe and consistent.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
