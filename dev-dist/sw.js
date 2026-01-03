c1318a911d984a8dc393473e32a7618463d8745b
Fatih Bıyıklı <fatihbiyiklio@hotmail.com>
2026-01-04 00:30:17 +0300

    chore: Update index.html precache revision in service worker.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u6o98dh05ig"
+    "revision": "0.k8bb5hp4th8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
